9f22a026c24682f0d5d4a8c817876dc8c9fd0744
Arya <aryakoukia@gmail.com>
2021-04-10 00:19:42 -0700

    Added google maps activity support, changed restaurant object to include Latitude and Longitude for map activity.

--- app/src/main/java/ca/bcit/restoraunt/Restaurant.java
+++ app/src/main/java/ca/bcit/restoraunt/Restaurant.java
@@ -6,15 +6,17 @@ public class Restaurant implements Serializable {
     private String name;
     private String address;
     private String imgURL;
+    private String latLong;
 
     public Restaurant() {
 
     }
 
-    public Restaurant(String name, String address, String imgURL){
+    public Restaurant(String name, String address, String imgURL, String latLong){
         this.name = name;
         this.address = address;
         this.imgURL = imgURL;
+        this.latLong = latLong;
     }
 
 
@@ -28,4 +30,5 @@ public class Restaurant implements Serializable {
 
     public String getImgURL() { return imgURL; }
 
+    public String getLatLong() { return latLong; }
 }
--- app/src/main/java/ca/bcit/restoraunt/RestaurantDetailActivity.java
+++ app/src/main/java/ca/bcit/restoraunt/RestaurantDetailActivity.java
@@ -2,9 +2,13 @@ package ca.bcit.restoraunt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -17,9 +21,18 @@ public class RestaurantDetailActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
         displayRestaurantDetails();
+        Button locationButton = findViewById(R.id.locationButton);
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(RestaurantDetailActivity.this, GoogleMapsActivity.class);
+                Bundle bundle = getIntent().getExtras();
+                i.putExtras(bundle);
+                startActivity(i);
+            }
+        });
     }
     private void displayRestaurantDetails() {
-        //Restaurant restaurant = (Restaurant) getIntent().getExtras().get("items");
         Restaurant restaurant = (Restaurant) getIntent().getSerializableExtra("items");
         String restaurantName = restaurant.getName();
 
@@ -30,5 +43,7 @@ public class RestaurantDetailActivity extends AppCompatActivity  {
         ImageView imageView = findViewById(R.id.restaurant_img_menu);
         Picasso.with(this).load(restaurant.getImgURL()).into((imageView));
 
+
     }
+
 }
